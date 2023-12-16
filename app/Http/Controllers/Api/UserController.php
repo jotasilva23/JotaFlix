ef3c28ecb816568b4c8986cedb625835e635e04d
jocelio <joceliotecinfor@alu.ufc.br>
2023-12-16 10:06:59 -0300

    fix/Inserção e listagem de usuários

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate();
+        return UserResource::collection($users);
+    }
+
+    public function store(StoreUpdateUserRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = bcrypt($request->password);
+        $user = User::create($data);
+
+        dd($data);
+
+        return new UserResource($user);
+    }
+
+    public function show(string $id)
+    {
+
+        $user = User::findOrFail($id);
+
+        return new UserResource($user);
+    }
+}
