26ceb9e72d118cdfcd6fbf5c06d7c8773eb121b1
jocelio <joceliotecinfor@alu.ufc.br>
2023-12-16 10:38:16 -0300

    fix/inserindo rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/', function(){
+    return response()->json([
+        'sucess' =>true
+    ]);
+});
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::post('/users', [UserController::class, 'store']);
+
+Route::get('/users/{id}', [UserController::class, 'show']);
